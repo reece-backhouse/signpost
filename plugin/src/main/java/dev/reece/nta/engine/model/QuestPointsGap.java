bd733ac388665781f9460ace4b04f6e93623dab9
reece-backhouse <reece.backhouse@gmail.com>
2026-09-07 13:43:43 +0100

    feat: plugin goal and gap model

---
+++ plugin/src/main/java/dev/reece/nta/engine/model/QuestPointsGap.java
@@ -0,0 +1,13 @@
+package dev.reece.nta.engine.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
+/** The player has fewer quest points than the goal requires. */
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class QuestPointsGap extends Gap
+{
+	int have;
+	int need;
+}
